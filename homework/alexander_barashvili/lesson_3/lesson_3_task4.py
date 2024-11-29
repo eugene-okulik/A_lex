46892a75920a08494e5082911c247f731e28ee69
Alexander Barashvili <point225@gmail.com>
2024-11-29 23:32:26 +0300

    Added 4 completed tasks from lesson 3

---
+++ homework/alexander_barashvili/lesson_3/lesson_3_task4.py
@@ -0,0 +1,10 @@
+import math
+
+leg_1 = 7
+leg_2 = 9
+
+gypotenuse = math.sqrt(leg_1**2 + leg_2**2)
+area = leg_1 * leg_2 / 2
+
+print(f'Гипотенуза нашего треугольника равна: {gypotenuse}\n'
+      f'Площадь нашего треугольника равна: {area}')
