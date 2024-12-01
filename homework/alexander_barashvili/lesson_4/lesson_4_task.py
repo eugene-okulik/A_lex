04c54c1cf5212566c70ce05a19b8415fae30fdf9
Alexander Barashvili <point225@gmail.com>
2024-12-01 12:59:26 +0300

    homework/alexander_barashvili/lesson_4/lesson_4_task.py

--- homework/alexander_barashvili/lesson_4/lesson_4_task.py
+++ homework/alexander_barashvili/lesson_4/lesson_4_task.py
@@ -6,7 +6,7 @@ my_dict = {'tuple': (1, 2, 3, 4, 5),
 print(my_dict['tuple'][-1])
 my_dict['list'].append(6)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 123
+my_dict['dict'][('i am a tuple',)] = 123
 my_dict['set'].add(6)
 my_dict['set'].pop()
 print(my_dict)
