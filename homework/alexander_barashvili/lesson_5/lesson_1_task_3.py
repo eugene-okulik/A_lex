f751fd48918f06e1334816bd85a971340cf294ab
Alexander Barashvili <point225@gmail.com>
2024-12-01 13:44:40 +0300

    fixed f-string

--- homework/alexander_barashvili/lesson_5/lesson_1_task_3.py
+++ homework/alexander_barashvili/lesson_5/lesson_1_task_3.py
@@ -1,5 +1,7 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
-
 subjects = ['math', 'biology', 'geography']
 
-print(f'Students {', '.join(students)} study these subjects: {', '.join(subjects)}')
+students_names = ', '.join(students)
+students_subjects = ', '.join(subjects)
+
+print(f'Students {students_names} study these subjects: {students_subjects}')
