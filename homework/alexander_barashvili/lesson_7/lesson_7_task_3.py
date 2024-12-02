4a6f6d6451adf0fd31c2cbfbd3c972f5e71a6465
Alexander Barashvili <point225@gmail.com>
2024-12-03 01:00:20 +0300

    fixed blanks in some lines again

--- homework/alexander_barashvili/lesson_7/lesson_7_task_3.py
+++ homework/alexander_barashvili/lesson_7/lesson_7_task_3.py
@@ -4,8 +4,10 @@ c = 'результат работы программы: 209'
 d = 'результат: 2'
 
 def print_summ_args(*args):
+
     for arg in args:
         arg = int(arg[arg.index(':') + 2:]) + 10
         print(arg)
+
 
 print_summ_args(a, b, c, d)
