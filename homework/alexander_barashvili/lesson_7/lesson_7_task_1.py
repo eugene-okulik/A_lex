13862ae8417bfbb4958be059fa29a3121a6d7302
Alexander Barashvili <point225@gmail.com>
2024-12-03 00:45:15 +0300

    Add completed tasks from lesson 7

---
+++ homework/alexander_barashvili/lesson_7/lesson_7_task_1.py
@@ -0,0 +1,11 @@
+happy_number = 777
+
+user_number = int(input("Hey buddy, try to guess the number and win a prize."
+                            "\nEnter the number and test your luck ;)\n"))
+
+while True:
+    if user_number == happy_number:
+        print('Oh yeah baby you did it!!!\nYou won respect!')
+        break
+    else:
+        user_number = int(input('Try again. I believe in you!!!\n'))
---
+++ homework/alexander_barashvili/lesson_7/lesson_7_task_3.py
@@ -0,0 +1,12 @@
+a = 'результат операции: 42'
+b = 'результат операции: 54'
+c = 'результат работы программы: 209'
+d = 'результат: 2'
+
+def print_summ_args(*args):
+    for arg in args:
+        arg = int(arg[arg.index(':') + 2:]) + 10
+        print(arg)
+
+
+print_summ_args(a, b, c, d)
